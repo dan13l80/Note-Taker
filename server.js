6b0f2b0a6b994d9ebcd8edd0ee7f8c1770292f6c
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2021-11-08 22:15:48 -0500

    Port created

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
