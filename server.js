73dfdb6792abb5c716a33cce20973b82eadfde8e
dan13l80 <88808310+dan13l80@users.noreply.github.com>
2021-11-08 22:20:45 -0500

    routes updated

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const apiRoutes = require("./routes/apiRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, function() {
+    console.log("Server is listening on https://localhost/" + PORT);
+})
